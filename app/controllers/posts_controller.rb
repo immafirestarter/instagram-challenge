1e0a8f7427918b4608f4a9bb3c72f2068b86568a
Sami <sami7mason@yahoo.co.uk>
2017-06-04 22:51:27 +0100

    image upload functionality added with paperclip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,20 @@ class PostsController < ApplicationController
 
   def show
   end
+
+  def create
+    @post = Post.new(allow_post)
+    if @post.save
+      flash[:success] = "Woot!"
+      redirect_to post_path(@post)
+    else
+      flash[:error] = @post.errors.full_messages
+      redirect_to new_post_path
+    end
+  end
+
+  private
+  def allow_post
+    params.require(:post).permit(:image, :description)
+  end  
 end
